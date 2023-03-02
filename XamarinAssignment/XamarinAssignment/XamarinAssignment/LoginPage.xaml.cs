4ab4fa73ad26b164c4df48f4ec9ee7dbd4a6d4b6
Hrishikesh-Baroori <125913941+Hrishi0198@users.noreply.github.com>
2023-03-02 18:57:05 +0530

    Day 3 Assignment

---
+++ XamarinAssignment/XamarinAssignment/XamarinAssignment/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinAssignment
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+    }
+}
+
